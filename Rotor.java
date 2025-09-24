e08121be2ccd6ad8e65e4d720842265a8a90be3e
Teni Sholarin <tenisholarin@MacBook-Pro.local>
2025-09-24 16:17:04 -0400

    Rotor is done

--- Rotor.java
+++ Rotor.java
@@ -3,27 +3,52 @@ public class Rotor {
     
     private String rotorValues;
     private char startChar;
-        
-    public Rotor(String v, char c){
-        this.rotorValues = new String(v);
-        this.startChar = c;
+    
+
+    public Rotor(String v, char c){ //  #GNUAHOVBIPWCJQXDKRYELSZFMT , #
+        this.rotorValues = new String(v); // #GNUAHOVBIPWCJQXDKRYELSZFMT
+        this.startChar = c; // #
         
         while(!this.rotate());
             
     }
     
     public boolean rotate(){
-        //TODO
-               
+        char[] rot = rotorValues.toCharArray(); //#GNUAHOVBIPWCJQXDKRYELSZFMT
+        char[] str = new char[rot.length];
+        str[0] = rot[rot.length - 1];    //T#GNUAHOVBIPWCJQXDKRYELSZFM
+        boolean match = false;
+        for (int i = 1; i < rot.length ; i++ ){
+            str[i] = rot[i - 1]; 
+        }
+
+        rotorValues = new String(str);
+
+        if (str[0] == startChar){
+            match = true;
+            return match;
+        }
+       return false;        
     }
     
 
     public int indexOf(char c){
-        //TODO
+        
+        char[] rot = rotorValues.toCharArray();
+        //System.out.println(rot[0]);
+        for (int i = 0; i < rot.length; i++){
+            if (rot[i] == c ) {
+                return i;
+            }
+        }
+        return -1;
     }
-
+    //Return the character at a given index.
     public char charAt(int idx){
-        //TODO
+       
+        char[] rot = rotorValues.toCharArray();
+        
+        return rot[idx];
     }
 }
     
